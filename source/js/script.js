c11c4e3c8c84548d235ea6af5e7c98670976755c
Yana <nuska13@mail.ru>
2020-05-02 11:20:15 +0300

    Продолжаем вёрстку личного проекта

---
+++ source/js/script.js
@@ -0,0 +1,19 @@
+var mainNav=document.querySelector(".main-nav");
+var mainNavButton=document.querySelector(".page-header__menu-toggle");
+mainNav.classList.remove("main-nav--nojs");
+mainNavButton.addEventListener("click",function() {
+  if (mainNav.classList.contains("main-nav--closed")) {
+    mainNav.classList.remove("main-nav--closed");
+    mainNav.classList.add("main-nav--opened");
+  } else {
+    mainNav.classList.add("main-nav--closed");
+    mainNav.classList.remove("main-nav--opened");
+  }
+  if (mainNavButton.classList.contains("page-header__menu-toggle--open")) {
+    mainNavButton.classList.remove("page-header__menu-toggle--open");
+    mainNavButton.classList.add("page-header__menu-toggle--close");
+  } else {
+    mainNavButton.classList.remove("page-header__menu-toggle--close");
+    mainNavButton.classList.add("page-header__menu-toggle--open");
+  }
+});
